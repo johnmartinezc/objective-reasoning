58e000370d6707c8300616db0627f5a1c23c25cb
johnmartinezc <john.martinez@codeimmersives.com>
2022-10-12 16:34:23 -0400

    -a

--- main.js
+++ main.js
@@ -25,22 +25,49 @@ function getEmail(user){
 }
 
 
-let ciUser ={
-    firstName: 'John',
-    lastName: 'Martinez'
+function getPlaylistLength(playList){
+    return playList.songs.length
 }
-
-
-console.log(getEmail(ciUser))
-
-
-
-
+// console.log(getPlaylistLength({
+//     name: 'my jamz',
+//     songs:['Linkin park', 'Black eye pea']
+// }))
 
 
 
 
+function getHardestHomework(homeWorkArray){
+    if(homeWorkArray.length === 0){
+        return ''
+    }
+  
+  
+    let lowestHw = homeWorkArray[0].avarageScore
+    let lowestHwName = homeWorkArray[0].name
+  
+    for(let i = 0; i < homeWorkArray.length; i++){
+        if(homeWorkArray[i].avarageScore < lowestHw.avarageScore){
+            lowestHw = homeWorkArray[i].avarageScore
+            lowestHwName = homeWorkArray[i].name
+        }
+    }
+    return lowestHw.name
+  }
+  
+  console.log(getHardestHomework([]))
+  
 
+
+  function createPhonebook(nameArray, numberArray){
+    let phoneBook ={}
+
+    for(let i = 0; i < nameArray.length; i++){
+        phoneBook[nameArray[i]] = numberArray[i]
+    }
+    return phoneBook
+  }
+  
+  console.log(createPhonebook(['melissa', 'Ander'],['206-111-1234', '123-543-9876']))
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
